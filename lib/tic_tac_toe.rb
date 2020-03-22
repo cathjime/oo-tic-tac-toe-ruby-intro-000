7b2b6b90605303f68cd382b13f05ae79ce076b8a
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-22 18:10:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,14 +4,14 @@ class TicTacToe
   end
 
   WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5],  # Middle row
-  [6,7,8], # bottom row
-  [0,3,6], #first column
-  [1,4,7], #second column
-  [2,5,8], #third column
-  [0,4,8], #diagonal1
-  [2,4,6] #diagonal2
+  [0,1,2], 
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
 ]
 
 def display_board(board)

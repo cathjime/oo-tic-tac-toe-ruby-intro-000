9f81661a4d477a8cf0ca2190d5579c47d5092741
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-22 18:38:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,7 +14,7 @@ class TicTacToe
   [2,4,6]
 ]
 
-def display_board(board = [" "," "," "," "," "," "," "," "," "])
+def display_board()
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
@@ -26,7 +26,7 @@ def input_to_index(user_input)
   user_input.to_i -  1
 end
 
-def move (board, index, value)
+def move (index, value)
   board[index] = value
 end
 

b23fd5d99fd76000e84a576470b9056308a9620b
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-22 17:58:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,5 +3,14 @@ class TicTacToe
     @board = board
   end
 
-  
+  WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8], # bottom row
+  [0,3,6], #first column
+  [1,4,7], #second column
+  [2,5,8], #third column
+  [0,4,8], #diagonal1
+  [2,4,6] #diagonal2
+]
 end

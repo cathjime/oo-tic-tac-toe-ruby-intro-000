e7dabb5f1f37f21a629efe07e772efe6eed11930
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-22 18:44:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,7 +61,7 @@ def turn()
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(index)
-    move(index, current_player(board))
+    move(index, current_player())
     display_board(board)
   else
     turn(board)

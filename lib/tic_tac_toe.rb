9f205b087f4081bb7d4779da4177cb3daa5ad2da
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-22 18:44:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,4 +56,16 @@ def current_player()
   end
 end
 
+def turn()
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
 end
